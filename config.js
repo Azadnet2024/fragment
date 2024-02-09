cabb5835a2d24b4204934242f38f7c8847f7fbf9
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-02-09 09:03:33 +0330

    Update config.js

--- config.js
+++ config.js
@@ -66,7 +66,7 @@ function generateConfig() {
         "outbounds": [
             {
                 "tag": "proxy",
-                "protocol": selectedProtocol === "trojan" ? "trojan" : (selectedProtocol === "vless" ? "vless" : "vmess"),
+                "protocol": selectedProtocol ? "vless" : "vmess",
                 "settings": {
                     "vnext": [
                         {
