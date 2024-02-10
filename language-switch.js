d61d6f72cdd22ff505139626c6fd85e00960436a
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-02-10 07:37:32 +0330

    Update language-switch.js

--- language-switch.js
+++ language-switch.js
@@ -1,13 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setLanguage('en');
+});
+
 function setLanguage(lang) {
     document.documentElement.lang = lang;
     updatePageContent(lang);
 }
 
 function updatePageContent(lang) {
+    document.documentElement.dir = lang === 'fa' ? 'rtl' : 'ltr';
+
     var elements = document.querySelectorAll('[data-en], [data-fa]');
-    elements.forEach(function (element) {
+
+    elements.forEach(function(element) {
         var isEnglish = lang === 'en';
-        var text = isEnglish ? element.getAttribute('data-en') : element.getAttribute('data-fa');
-        element.textContent = text;
+        var content = isEnglish ? element.getAttribute('data-en') : element.getAttribute('data-fa');
+        element.textContent = content;
     });
 }
