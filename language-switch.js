ea14e408267c16b1c98ce48782e1ad41ac29543a
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-02-10 05:44:20 +0330

    Update language-switch.js

--- language-switch.js
+++ language-switch.js
@@ -1,31 +1,20 @@
-// language-switch.js
-
 function setLanguage(lang) {
-    document.documentElement.lang = lang; // Set the HTML lang attribute
+    document.documentElement.lang = lang;
 
-    // Call functions to update content based on the selected language
     updatePageContent(lang);
 }
 
 function updatePageContent(lang) {
-    // Get the elements by ID
     var engData = document.getElementById('eng-data');
     var faData = document.getElementById('fa-data');
 
-    // Check if the elements exist before accessing their style properties
     if (engData && faData) {
-        // Implement logic to update specific elements based on the language
-        // For example, you can toggle visibility or change text content
         if (lang === 'fa') {
-            // Update elements for Farsi
             engData.style.display = 'none';
             faData.style.display = 'block';
         } else {
-            // Update elements for English
             engData.style.display = 'block';
             faData.style.display = 'none';
         }
     }
 }
-
-// You can add more functions or customize the behavior based on your needs
