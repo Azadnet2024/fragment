3f815c3f12c712880e610a6ea7ff750371573fbf
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-02-10 06:18:29 +0330

    Update language-switch.js

--- language-switch.js
+++ language-switch.js
@@ -1,32 +1,13 @@
-"use strict";
 function setLanguage(lang) {
     document.documentElement.lang = lang;
     updatePageContent(lang);
 }
 
 function updatePageContent(lang) {
-    var engData = document.getElementById('eng-data');
-    var faData = document.getElementById('fa-data');
-
-    if (engData && faData) {
-        if (lang === 'fa') {
-            toggleContentVisibility(engData, false);
-            toggleContentVisibility(faData, true);
-        } else {
-            toggleContentVisibility(engData, true);
-            toggleContentVisibility(faData, false);
-        }
-    }
-}
-
-function toggleContentVisibility(element, isVisible) {
-    // You can use CSS classes for better separation of styles and behavior
-    // Add/Remove the appropriate class based on the visibility status
-    if (isVisible) {
-        element.classList.add('visible');
-        element.classList.remove('hidden');
-    } else {
-        element.classList.add('hidden');
-        element.classList.remove('visible');
-    }
+    var elements = document.querySelectorAll('[data-en], [data-fa]');
+    elements.forEach(function (element) {
+        var isEnglish = lang === 'en';
+        var text = isEnglish ? element.getAttribute('data-en') : element.getAttribute('data-fa');
+        element.textContent = text;
+    });
 }
